feecf3afde567c7372c08a0a15157cbe34c57fff
Weerawut <weerawut@live.com>
2024-12-16 09:24:21 +0700

    Initial Commit

---
+++ TextExploration/TextExplorationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextExplorationApp.swift
+//  TextExploration
+//
+//  Created by Weerawut Chaiyasomboon on 16/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct TextExplorationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
